198bcd5ce20e6a4d6cccc9a4a16da7fdbb8a3f29
Cameron Spickert <cspickert@gmail.com>
2013-02-04 14:09:31 -0500

    Initial Commit

---
+++ Photos/CSAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CSAppDelegate.h
+//  Photos
+//
+//  Created by Cameron Spickert on 2/4/13.
+//  Copyright (c) 2013 Cameron Spickert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Photos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photos
+//
+//  Created by Cameron Spickert on 2/4/13.
+//  Copyright (c) 2013 Cameron Spickert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CSAppDelegate class]));
+    }
+}
